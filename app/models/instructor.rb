d0989fac5cc7e29e68cfa97a3b31b4eb420de91e
Bill <billfeng2002@gmail.com>
2021-06-14 13:05:32 -0700

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,37 @@
 class Instructor
+  @@all = []
+  def self.all
+    @@all
+  end
 
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def passed_students
+    BoatingTest.all.select { |test| (test.instructor == self) && (test.test_status == "passed") }
+  end
+
+  def pass_student(student, test_name)
+    test = BoatingTest.all.find { |test| (test.instructor == self) && (test.test_name == test_name) && (test.student==student)}
+    unless test == nil
+      test.test_status = "passed"; return test
+    end
+    BoatingTest.new(student, test_name, "passed", self)
+  end
+
+  def fail_student(student, test_name)
+    test = BoatingTest.all.find { |test| (test.instructor == self) && (test.test_name == test_name) && (test.student == student)}
+    unless test == nil
+      test.test_status = "failed"; return test
+    end
+    BoatingTest.new(student, test_name, "failed", self)
+  end
+
+  def all_students
+    BoatingTest.all.select { |test| test.instructor == self }.map{|test| test.student}
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,31 @@
 class Student
+    @@all=[]
+    attr_reader :name
+    def self.all
+        @@all
+    end
+    def initialize(first_name)
+        @name=first_name
+        @@all << self
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def all_instructors
+        self.get_tests.map{|test|test.instructor}
+    end
+
+    def get_tests
+        BoatingTest.all.select{|test| test.student==self}
+    end
+    def grade_percentage
+        self.get_tests.count{|test|test.test_status=="passed"}.to_f/self.get_tests.count * 100
+    end
+
+    def self.find_student(name)
+        self.all.find{|student| student.name==name}
+    end
 
 end
